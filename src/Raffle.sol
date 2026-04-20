5d83bd0a394c917b4b64ef02d33c7f17b0b13036
buhuizuocaigou <95681626+buhuizuocaigou@users.noreply.github.com>
2026-04-20 15:13:39 +0800

    进程测试更改结束

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -225,4 +225,8 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
     function getRaffleState() public view returns (RaffleState) {
         return s_raffleState;
     }
+    function getPlayer(uint256 indexOfPlayer) external view returns (address) {
+        return s_players[indexOfPlayer];
+    }
 }
+//
--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -48,11 +48,23 @@ contract RaffleTest is Test {
     //     s_players.push(payable(msg.sender)); //推送抽奖者到这个数组内
     //     emit EnteredRaffle(msg.sender); //设置emit 的是为了将链内的事件同步到链外去做，并且这个事件不写入一个storage内
     // }
-
+    //想到如果支付金额不足是否可能被取消的问题：
     function testRaffleRevertsWhenYouDontPayEnough() public {
         vm.prank(PLAYER); //借助player 这个身份
 
         vm.expectRevert(Raffle.Raffle__NotEnoughETHEntered.selector); //看看到底有没有回滚 .selector是意味着evmYOGN 用字节表现错误的那种机制代码 evm只认字节 而 .selector是将他编译成字节的模型 这样子能让EVM底层认识到这个
         raffle.enterRaffle();
+    }
+    //测试合约到底是否会如实的像raffle.sol中添加player
+
+    function testRaffleRecordsPlayersWhenTheyEnter() public {
+        //Arrage
+        vm.prank(PLAYER);
+
+        raffle.enterRaffle{value: entranceFee}(); //以player的身份参与抽奖
+
+        address playerRecorded = raffle.getPlayer(0); //由于有一个用户目的是 获取第一个玩家的地址 数组为0的地址
+
+        assert(playerRecorded == PLAYER); //我预计 这里使用的人是player ，然后如果不是则在我这个失败提示截断
     }
 }
